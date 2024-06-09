20f96a88f7d6ebb5b13a7deb080779143b090331
capricorn <8250820+capricorn@users.noreply.github.com>
2024-06-09 09:15:09 -0600

    Add graph label denoting convolution parameters

--- make_graph.py
+++ make_graph.py
@@ -2,10 +2,12 @@ from jinja2 import Template
 
 import conv
 
-all_convs = conv.convolutions(2,10)
+k = 2
+n = 10
+all_convs = conv.convolutions(k,n)
 edges = conv.n2(all_convs)
 
 with open('graph.jinja2', 'r') as f:
     t = Template(f.read())
 
-print(t.render(edges=edges))
+print(t.render(edges=edges,k=k,n=n))
